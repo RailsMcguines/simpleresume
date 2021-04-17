510a803d5c224b84b6438518ae66d2421fda0623
Enzo Rodriguez <lab.er.intro@gmail.com>
2021-04-17 17:19:02 -0300

    third fix for db routes

--- js/query.js
+++ js/query.js
@@ -1,21 +1,36 @@
 $(document).ready(function() {
 
-    $.get("../../js/fakedb.json",function(data){
-        let record ='<article class="mt-0 mx-auto">';
-        $.each( data, function( i ) {                    
-        let short= data[i].content.split(" ").splice(0,18).join(" ");
+  var url=window.location.href;
 
-                record=`<h6 style="line-height:18px; font-weight:600; padding:0;">${data[i].title}</h6>
-                    <p style="font-size:11px; padding-left:3px; margin:-4px;">${data[i].date}</p>
-                    <p style="font-size: 12px; line-height:14px; margin-bottom:0px; margin-top:8px;"> ${short}  </p>
-                    <a href="#" style="color:#2B353F !important; font-size:14px; font-weight:bold; margin-top:5px;">Ver más...</a>
-                    </article>  
-                    <hr class="dashed">`;
-            $("#news").append(record); 
+    thisString = new RegExp('\\b' + 'index' + '\\b', 'i');
+     if(thisString.test(url)){
+    consulta("js/fakedb.json");
+        }
+    else {	
+    consulta("../../js/fakedb.json");
+    
+     }
+    
+    function consulta(dbr){
+             $.get(dbr,function(data){
+                 
+            let record ='<article class="mt-0 mx-auto">';
+            $.each( data, function( i ) {                    
+            let short= data[i].content.split(" ").splice(0,18).join(" ");
+    
+                    record=`<h6 style="line-height:18px; font-weight:600; padding:0;">${data[i].title}</h6>
+                        <p style="font-size:11px; padding-left:3px; margin:-4px;">${data[i].date}</p>
+                        <p style="font-size: 12px; line-height:14px; margin-bottom:0px; margin-top:8px;"> ${short}  </p>
+                        <a href="#" style="color:#2B353F !important; font-size:14px; font-weight:bold; margin-top:5px;">Ver más...</a>
+                        </article>  
+                        <hr class="dashed">`;
+                    $("#news").append(record); 
+               
+            }); 
            
-        }); 
-       
-    });
+        });
+        
+    }  
       
     document.getElementById("accordionFlushExample").addEventListener("click", function(e){
 
