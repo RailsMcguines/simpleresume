e68bcc0996873aab8452934637eae923978b48e5
Enzo Rodriguez <lab.er.intro@gmail.com>
2021-04-17 15:38:06 -0300

    fix js and images routes

--- js/query.js
+++ js/query.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $.get("../../js/fakedb.json",function(data){
+    $.get("/fakedb.json",function(data){
         let record ='<article class="mt-0 mx-auto">';
         $.each( data, function( i ) {                    
         let short= data[i].content.split(" ").splice(0,18).join(" ");
